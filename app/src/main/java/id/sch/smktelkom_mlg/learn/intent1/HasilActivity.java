d0b5265cec4f3f83d00bcd80902dc6f6dfca3c26
Vejryn Shaviero <envyero@gmail.com>
2016-10-05 22:33:12 +0700

    startActivity Part 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -0,0 +1,23 @@
+package id.sch.smktelkom_mlg.learn.intent1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HasilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,HasilActivity.class));
+            }
+        });
+    }
+}
